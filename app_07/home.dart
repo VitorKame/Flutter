39e95df0e90a502f49dc21c74ebd5a0e248a99a9
VitorKame <santos.vitor326@gmail.com>
2022-04-08 22:22:35 -0300

    Add files via upload

--- app_07/home.dart
+++ app_07/home.dart
@@ -8,100 +8,172 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String dropdownValor = 'Sexo';
+  String dropdownValorEscolaridade = 'Escolaridade';
+  double valorSlider = 100;
+  bool valorSwitch = false;
+  String Resposta = "";
+
+  TextEditingController n1Controller = TextEditingController();
+  TextEditingController n2Controller = TextEditingController();
+
+  void _ExibirTexto() {
+    setState(() {
+      Resposta =
+          "Nome: ${n1Controller.text}\nIdade: ${n2Controller.text}\nEscolaridade: ${dropdownValorEscolaridade}\nLimite: ${valorSlider}\nBrasileiro: ${valorSwitch ? 'sim' : 'não'}";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: _titulo(),
-        backgroundColor: Colors.white,
-        body: _corpo(),
-      ),
+    return Scaffold(
+      appBar: _titulo(),
+      backgroundColor: Colors.white,
+      body: _corpo(),
     );
   }
 
   _titulo() {
     return AppBar(
-      title: Text(
-        'Anúncios',
-        style: TextStyle(color: Colors.red, fontSize: 25),
-      ),
+      title: Text("Abertura de Conta"),
       centerTitle: true,
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.green,
     );
   }
 
   _corpo() {
-    return Container(
-      color: Colors.white,
-      padding: const EdgeInsets.all(10.0),
-      child: Container(
-        color: Colors.yellow,
-        child: Column(
-          children: <Widget>[
-            _carrosel(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  _carrosel() {
-    return Container(
-      margin: EdgeInsets.only(top: 20, bottom: 20),
-      height: 452,
-      child: PageView(
+    return SingleChildScrollView(
+      padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          for (int i = 0; i < 5; i++) _containerAnuncio(),
+          _campo("Nome:", n1Controller),
+          _campo("Idade:", n2Controller),
+          _dropdownSexo(),
+          _dropdownEscolaridade(),
+          Text(
+            'Limite:',
+            textAlign: TextAlign.left,
+            style: TextStyle(color: Colors.black, fontSize: 20.0),
+          ),
+          _slider(),
+          Text(
+            'Brasileiro:',
+            textAlign: TextAlign.left,
+            style: TextStyle(color: Colors.black, fontSize: 20.0),
+          ),
+          _switch(),
+          _botao(),
+          _Resposta(Resposta),
         ],
       ),
     );
   }
 
-  _containerAnuncio() {
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(left: 20, right: 20),
-          padding: const EdgeInsets.all(10.0),
-          decoration: BoxDecoration(
-            border: Border.all(),
-          ),
-          child: Column(
-            children: <Widget>[
-              _foto2('rock1.jpg'),
-              _texto(),
-            ],
-          ),
-        ),
-      ],
+  _campo(labelTitulo, objController) {
+    return TextField(
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+          labelText: labelTitulo, labelStyle: TextStyle(color: Colors.black)),
+      textAlign: TextAlign.left,
+      style: TextStyle(color: Colors.black, fontSize: 20.0),
+      controller: objController,
     );
   }
 
-  _foto2(String nomeFoto) {
+  _dropdownSexo() {
+    return DropdownButton<String>(
+      value: dropdownValor,
+      items: <String>['Sexo', 'Masculino', 'Feminino', 'Outro']
+          .map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      onChanged: (String? valorSelecionado) {
+        setState(() {
+          dropdownValor = valorSelecionado!;
+        });
+      },
+    );
+  }
+
+  _dropdownEscolaridade() {
+    return DropdownButton<String>(
+      value: dropdownValorEscolaridade,
+      items: <String>[
+        'Escolaridade',
+        'Ensino Médio Completo',
+        'Ensino Superior Completo',
+        'Ensino Médio Incompleto'
+      ].map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      onChanged: (String? valorSelecionado) {
+        setState(() {
+          dropdownValorEscolaridade = valorSelecionado!;
+        });
+      },
+    );
+  }
+
+  _slider() {
+    return Slider(
+      value: valorSlider,
+      activeColor: Colors.green,
+      min: 0,
+      max: 100,
+      divisions: 10,
+      label: valorSlider.round().toString(),
+      onChanged: (double value) {
+        setState(() {
+          valorSlider = value;
+        });
+      },
+    );
+  }
+
+  _switch() {
+    return Switch(
+      value: valorSwitch,
+      onChanged: (value) {
+        setState(() {
+          valorSwitch = value;
+        });
+      },
+      activeTrackColor: Colors.lightGreen,
+      activeColor: Colors.green,
+    );
+  }
+
+  _botao() {
     return Padding(
-      padding: EdgeInsets.all(30),
-      child: Image.asset(
-        "assets/images/$nomeFoto",
-        height: 150,
-        width: 150,
-        //fit: BoxFit.fill,
-        //fit: BoxFit.contain,
-        fit: BoxFit.cover,
+      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: Container(
+        height: 50.0,
+        child: RaisedButton(
+          onPressed: _ExibirTexto,
+          child: Text("Cadastrar",
+              style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          color: Colors.green,
+        ),
       ),
     );
   }
 
-  _texto() {
-    return const Text(
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  _Resposta(Resposta) {
+    return Text(
+      Resposta,
+      textAlign: TextAlign.left,
       style: TextStyle(
-          color: Colors.blue,
-          fontSize: 10,
-          fontWeight: FontWeight.bold,
-          fontStyle: FontStyle.italic,
+          color: Colors.black,
+          fontSize: 22.0,
           decoration: TextDecoration.underline,
-          decorationColor: Colors.red,
+          decorationColor: Colors.lightGreen,
           decorationStyle: TextDecorationStyle.wavy),
     );
   }

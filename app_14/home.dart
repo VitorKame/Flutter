155f1c8ef58f3943b2a67797046dd5f15954dc95
VitorKame <santos.vitor326@gmail.com>
2022-06-09 01:37:13 -0300

    Add files via upload

---
+++ app_14/home.dart
@@ -0,0 +1,173 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String _textoSalvo = "Nada salvo!";
+  String textoSalvo = "";
+  String temaSalvo = "";
+  bool valorDia = false;
+  bool valorPequeno = false;
+  TextStyle estiloTexto = TextStyle(fontSize: 25, color: Colors.white);
+  TextStyle colorContainer = TextStyle(color: Colors.black);
+
+  TextEditingController _controllerCampo = TextEditingController();
+
+  _salvar() async {
+    String valorDigitado = _controllerCampo.text;
+    String estiloTextoDefinido = valorPequeno.toString();
+    String colorContainerDefinido = valorDia.toString();
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("nome", valorDigitado);
+    await prefs.setString("texto", estiloTextoDefinido);
+    await prefs.setString("tema", colorContainerDefinido);
+  }
+
+  _recuperar() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      _textoSalvo = prefs.getString("nome").toString();
+
+      textoSalvo = prefs.getString("texto").toString();
+      temaSalvo = prefs.getString("tema").toString();
+
+	valorPequeno = textoSalvo == "false" ? false : true;
+	valorDia = temaSalvo == "false" ? false : true;
+
+    });
+
+    print("Método Recuperar:  $_textoSalvo");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Visualização de Frases"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Tema dia:",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Switch(
+                        value: valorDia,
+                        activeTrackColor: Colors.blue,
+                        activeColor: Colors.blue,
+                        onChanged: (value) {
+                          setState(() {
+                            valorDia = value;
+                            if (valorDia == true) {
+                              valorDia = value;
+                              colorContainer = TextStyle(color: Colors.white);
+                              estiloTexto = TextStyle(
+                                  color: Colors.black,
+                                  fontSize: estiloTexto.fontSize);
+                            } else if (valorDia == false) {
+                              valorDia = value;
+                              colorContainer = TextStyle(color: Colors.black);
+                              estiloTexto = TextStyle(
+                                  color: Colors.white,
+                                  fontSize: estiloTexto.fontSize);
+                            }
+                          });
+                        }),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text(
+                      "Texto menor:",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Switch(
+                        value: valorPequeno,
+                        activeTrackColor: Colors.black,
+                        activeColor: Colors.black,
+                        onChanged: (value) {
+                          setState(() {
+                            valorPequeno = value;
+
+                            if (valorPequeno == true) {
+                              valorPequeno = value;
+                              estiloTexto = TextStyle(
+                                  fontSize: 15, color: estiloTexto.color);
+                            } else if (valorPequeno == false) {
+                              valorPequeno = value;
+                              estiloTexto = TextStyle(
+                                  fontSize: 25, color: estiloTexto.color);
+                            }
+                          });
+                        }),
+                  ],
+                ),
+              ],
+            ),
+            Card(
+                color: colorContainer.color,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    style: estiloTexto,
+                    maxLines: 15,
+                    decoration: InputDecoration(
+                        labelText: "Insira sua frase aqui!",
+                        fillColor: Colors.lightGreenAccent),
+                    controller: _controllerCampo,
+                    keyboardType: TextInputType.text,
+                  ),
+                )),
+
+            Row(children: <Widget>[
+              RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: EdgeInsets.all(25),
+                child: Text("Salvar", style: TextStyle(fontSize: 15)),
+                onPressed: _salvar,
+              ),
+              RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                padding: EdgeInsets.all(25),
+                child: Text("Recuperar", style: TextStyle(fontSize: 15)),
+                onPressed: _recuperar,
+              ),
+            ]),
+
+            Container(
+              color: colorContainer.color,
+              child: Text(
+                _textoSalvo,
+                style: estiloTexto,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _remover() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.remove("nome");
+
+    print("Método Remover");
+  }
+}
